795e022c0f9f3859dfcf82770dea7198015497f5
System Administrator <zaagagon@gmail.com>
2022-03-12 15:40:56 -0500

    cambios

--- plus.php
+++ plus.php
@@ -6,6 +6,7 @@
   $N1=$_POST['N1'];//creacion de la variable php
   $N2=$_POST['N2'];
   $total=$N1+$N2;
+  //$total=20;
  //$total=$n1+$n2;
  echo "El total de la suma es: ".$total.".\n";
  echo "<br>";
