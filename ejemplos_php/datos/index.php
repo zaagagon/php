51ef78b7e1b16047a18a0322fb2629c2e21f0424
zaagagon <zaagagon@gmail.com>
2023-11-23 11:44:13 -0500

    datos insertar en php

---
+++ ejemplos_php/datos/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Registro</title>
+</head>
+<body>
+    <h2>Formulario de Registro</h2>
+
+    <form action="insertar.php" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" required><br>
+
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" required><br>
+
+        <label for="correo">Correo:</label>
+        <input type="email" name="correo" required><br>
+
+        <label for="contrasena">Contraseña:</label>
+        <input type="password" name="contrasena" required><br>
+
+        <input type="submit" value="Registrar">
+    </form>
+</body>
+</html>
