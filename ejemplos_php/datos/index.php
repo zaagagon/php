beab9645e9d929e766cada6f228dc541d65b9a36
zaagagon <zaagagon@gmail.com>
2023-11-27 23:52:45 -0500

    revisar conexion

--- ejemplos_php/datos/index.php
+++ ejemplos_php/datos/index.php
@@ -23,5 +23,53 @@
 
         <input type="submit" value="Registrar">
     </form>
+
+    <!-- Lista de Usuarios -->
+    <h2 class="mb-4">Lista de Usuarios</h2>
+
+<?php
+// Conexión a la base de datos (ajusta los valores según tu configuración)
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$dbname = "prueba";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+// Consultar todos los usuarios
+$result = $conn->query("SELECT * FROM usuarios");
+
+if ($result->num_rows > 0) {
+    echo "<ul class='list-group'>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li class='list-group-item d-flex justify-content-between align-items-center'>
+                <span>ID: {$row['id']}, Nombre: {$row['nombre']}, Apellido: {$row['apellido']}, Correo: {$row['correo']}</span>
+                <form action='borrar.php' method='post'>
+                    <input type='hidden' name='id' value='{$row['id']}'>
+                    <button type='submit' class='btn btn-danger btn-sm'>Borrar</button>
+                </form>
+              </li>";
+    }
+    echo "</ul>";
+} else {
+    echo "No hay usuarios registrados";
+}
+
+// Cerrar conexión
+$conn->close();
+?>
+
+</div>
+
+<!-- Scripts de Bootstrap (jQuery y Popper.js son requeridos) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
