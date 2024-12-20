1e272817acba8441159c36ca654e3ae939e58fbf
zaagagon <zaagagon@gmail.com>
2024-12-19 21:46:21 -0500

    arreglo convertido a cadena con implode

--- ejemplos_php/arrays_php/array2_index.php
+++ ejemplos_php/arrays_php/array2_index.php
@@ -9,6 +9,14 @@
     <h1>ARREGLOS INDEXADOS RECORRIDOS</h1>
     <?php
 
+    $personajes=["lolo","yolis","mako"];
+
+    //Para arreglos indexados, implode funciona directamente, uniendo los valores de los elementos en una cadena.
+
+    //creamos cadena
+    $cadena = implode(', ',$personajes);
+    echo $cadena;
+
     ?>
 </body>
 </html>
