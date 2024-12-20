6441b72c78938c3bc6987e8f49f6da14d289623d
zaagagon <zaagagon@gmail.com>
2024-12-19 21:09:08 -0500

    array ok pero como cadena ojo

--- ejemplos_php/arrays_php/array_indexado.php
+++ ejemplos_php/arrays_php/array_indexado.php
@@ -1,6 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 //array indexado
 //Son listas donde los índices son numéricos y comienzan desde 0.
 
 
 $personajes=["Deadpool","lovezno","viuda negra"];
+
+echo $personajes;
+?>
+</body>
+</html>
+
