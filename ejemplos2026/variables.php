3be2bf66176e0fc7d27017c78210026b51fa25ca
zaagagon <zaagagon@gmail.com>
2026-05-26 12:12:51 -0500

    nuevos cambios con variables

--- ejemplos2026/variables.php
+++ ejemplos2026/variables.php
@@ -3,3 +3,9 @@
 $nombre = "TecnoIA";
 $edad = 5;
 $precio = 19.99;
+
+//no va a mostrar el tipo de dato, solo el valor
+echo "datos de la variable nombre: ". "<br>";
+echo "practicando variables con php"."<br>";
+
+echo "Nombre: " . $nombre . "<br>";
