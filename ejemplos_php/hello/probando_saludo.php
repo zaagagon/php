af55046426c8284dfb14a0d15a252f5d94bcc42f
zaagagon <zaagagon@gmail.com>
2026-06-02 12:24:00 -0500

    practicando con php y html

---
+++ ejemplos_php/hello/probando_saludo.php
@@ -0,0 +1,2 @@
+<?php
+echo "Hola, bienvenido a TecnoIA";
---
+++ ejemplos_php/hello/sumar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Declaración de variables
+        $a = 5;
+        $b = 10;
+        // Declarar las variables $a y $b, y asignarles los valores 5 y 10 respectivamente
+        $suma = $a + $b;
+        // Realizar la suma de $a y $b, y almacenar el resultado en $suma
+        echo "La suma de $a y $b es: $suma";
+        ?>
+        <h2>Resultado de la suma :</h2>
+</body>
+</html>
